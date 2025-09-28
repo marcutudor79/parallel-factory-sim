d5e07a3b9575960b93608ec7ccfcc0326221ea90
marcutudor79 <marcutudor5@protonmail.com>
2025-09-28 14:23:19 +0200

    Initial commit

---
+++ code/robotic-factory-simulator/fr.tp.inf112.projects.robotsim/src/fr/tp/inf112/projects/robotsim/model/path/SquareVertex.java
@@ -0,0 +1,27 @@
+package fr.tp.inf112.projects.robotsim.model.path;
+
+import fr.tp.inf112.projects.graph.impl.GridVertex;
+import fr.tp.inf112.projects.robotsim.model.Position;
+import fr.tp.inf112.projects.robotsim.model.shapes.RectangularShape;
+
+public class SquareVertex extends GridVertex {
+	
+	private final RectangularShape shape;
+
+	public SquareVertex(final String label,
+						final int xCoordinate,
+						final int yCoordinate,
+						final int size) {
+		super(label, xCoordinate, yCoordinate);
+
+		this.shape = new RectangularShape(xCoordinate, yCoordinate, size, size);
+	}
+
+	public RectangularShape getShape() {
+		return shape;
+	}
+	
+	public Position getPosition() {
+		return getShape() == null ? null : getShape().getPosition();
+	}
+}
