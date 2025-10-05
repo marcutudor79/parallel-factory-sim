a42e58fcf98b9d33a6de29751b148eb048248297
marcutudor79 <marcutudor5@protonmail.com>
2025-10-05 21:40:04 +0200

    2-data-persist: Improve SimulatorApplication comments
    Signed-off-by: marcutudor79 <marcutudor5@protonmail.com>

--- code/robotic-factory-simulator/fr.tp.inf112.projects.robotsim/src/fr/tp/inf112/projects/robotsim/app/SimulatorApplication.java
+++ code/robotic-factory-simulator/fr.tp.inf112.projects.robotsim/src/fr/tp/inf112/projects/robotsim/app/SimulatorApplication.java
@@ -28,66 +28,90 @@ import fr.tp.inf112.projects.robotsim.model.shapes.RectangularShape;
 
 public class SimulatorApplication {
 
-	public static void main(String[] args) {
-		
-		/* Instantiate logger */ 
-		final Logger LOGGER = Logger.getLogger(SimulatorApplication.class.getName());
-		LOGGER.info("Starting the robot simulator..");
-		LOGGER.config("With parameters " + Arrays.toString(args) + ".");
-		
-		final Factory factory = new Factory(200, 200, "Simple Test Puck Factory");
-		final Room room1 = new Room(factory, new RectangularShape(20, 20, 75, 75), "Production Room 1");
-		new Door(room1, Room.WALL.BOTTOM, 10, 20, true, "Entrance");
-		final Area area1 = new Area(room1, new RectangularShape(35, 35, 50, 50), "Production Area 1");
-		final Machine machine1 = new Machine(area1, new RectangularShape(50, 50, 15, 15), "Machine 1");
+    public static void main(String[] args) {
+        
+        /* Instantiate logger */ 
+        final Logger LOGGER = Logger.getLogger(SimulatorApplication.class.getName());
+        LOGGER.info("Starting the robot simulator..");
+        LOGGER.config("With parameters " + Arrays.toString(args) + ".");
+        
+        /* Create factory with rooms model */
+        final Factory factory  = new Factory(200, 200, "Simple Test Puck Factory");
+        
+        /* Create room1 */
+        final Room room1       = new Room(factory, new RectangularShape(20, 20, 
+                                          75, 75), "Production Room 1");
+                                 new Door(room1, Room.WALL.BOTTOM, 10, 20, true, 
+                                          "Entrance");
+        final Area area1       = new Area(room1, new RectangularShape(35, 35, 
+                                          50, 50), "Production Area 1");
+        final Machine machine1 = new Machine(area1, new RectangularShape(50, 50, 
+                                          15, 15), "Machine 1");
 
-		final Room room2 = new Room(factory, new RectangularShape( 120, 22, 75, 75 ), "Production Room 2");
-		new Door(room2, Room.WALL.LEFT, 10, 20, true, "Entrance");
-		final Area area2 = new Area(room2, new RectangularShape( 135, 35, 50, 50 ), "Production Area 1");
-		final Machine machine2 = new Machine(area2, new RectangularShape( 150, 50, 15, 15 ), "Machine 1");
-		
-		final int baselineSize = 3;
-		final int xCoordinate = 10;
-		final int yCoordinate = 165;
-		final int width =  10;
-		final int height = 30;
-		final BasicPolygonShape conveyorShape = new BasicPolygonShape();
-		conveyorShape.addVertex(new BasicVertex(xCoordinate, yCoordinate));
-		conveyorShape.addVertex(new BasicVertex(xCoordinate + width, yCoordinate));
-		conveyorShape.addVertex(new BasicVertex(xCoordinate + width, yCoordinate + height - baselineSize));
-		conveyorShape.addVertex(new BasicVertex(xCoordinate + width + baselineSize, yCoordinate + height - baselineSize));
-		conveyorShape.addVertex(new BasicVertex(xCoordinate + width + baselineSize, yCoordinate + height));
-		conveyorShape.addVertex(new BasicVertex(xCoordinate - baselineSize, yCoordinate + height));
-		conveyorShape.addVertex(new BasicVertex(xCoordinate - baselineSize, yCoordinate + height - baselineSize));
-		conveyorShape.addVertex(new BasicVertex(xCoordinate, yCoordinate + height - baselineSize));
+        /* Create room2 */
+        final Room room2       = new Room(factory, new RectangularShape( 120, 22, 
+                                          75, 75 ), "Production Room 2");
+                                 new Door(room2, Room.WALL.LEFT, 10, 20, true,
+                                          "Entrance");
+        final Area area2       = new Area(room2, new RectangularShape( 135, 35, 
+                                          50, 50 ), "Production Area 1");
+        final Machine machine2 = new Machine(area2, new RectangularShape( 150, 50, 
+                                          15, 15 ), "Machine 1");
+        
+        /* Create conveyor belt in the factory */
+        final int baselineSize = 3;
+        final int xCoordinate = 10;
+        final int yCoordinate = 165;
+        final int width =  10;
+        final int height = 30;
+        final BasicPolygonShape conveyorShape = new BasicPolygonShape();
+        conveyorShape.addVertex(new BasicVertex(xCoordinate, yCoordinate));
+        conveyorShape.addVertex(new BasicVertex(xCoordinate + width, yCoordinate));
+        conveyorShape.addVertex(new BasicVertex(xCoordinate + width, yCoordinate + height - baselineSize));
+        conveyorShape.addVertex(new BasicVertex(xCoordinate + width + baselineSize, yCoordinate + height - baselineSize));
+        conveyorShape.addVertex(new BasicVertex(xCoordinate + width + baselineSize, yCoordinate + height));
+        conveyorShape.addVertex(new BasicVertex(xCoordinate - baselineSize, yCoordinate + height));
+        conveyorShape.addVertex(new BasicVertex(xCoordinate - baselineSize, yCoordinate + height - baselineSize));
+        conveyorShape.addVertex(new BasicVertex(xCoordinate, yCoordinate + height - baselineSize));
 
-		final Room chargingRoom = new Room(factory, new RectangularShape(125, 125, 50, 50), "Charging Room");
-		new Door(chargingRoom, Room.WALL.RIGHT, 10, 20, false, "Entrance");
-		final ChargingStation chargingStation = new ChargingStation(factory, new RectangularShape(150, 145, 15, 15), "Charging Station");
+        /* Create a charging room */
+        final Room chargingRoom               = new Room(factory, new RectangularShape(125, 
+                                                         125, 50, 50), "Charging Room");
+                                                new Door(chargingRoom, Room.WALL.RIGHT, 10, 
+                                                         20, false, "Entrance");
+        final ChargingStation chargingStation = new ChargingStation(factory, new RectangularShape(150, 
+                                                         145, 15, 15), "Charging Station");
 
-		final FactoryPathFinder jgraphPahtFinder = new JGraphTDijkstraFactoryPathFinder(factory, 5);
-		final Robot robot1 = new Robot(factory, jgraphPahtFinder, new CircularShape(5, 5, 2), new Battery(10), "Robot 1");
-		robot1.addTargetComponent(machine1);
-		robot1.addTargetComponent(machine2);
-		//robot1.addTargetComponent(new Conveyor(factory, conveyorShape, "Conveyor 1"));
-		//robot1.addTargetComponent(chargingStation);
+        /* Create Djisktra path finder to be used by robots */
+        final FactoryPathFinder jgraphPahtFinder = new JGraphTDijkstraFactoryPathFinder(factory, 5);
+        
+        /* Create robots */
+        final Robot robot1 = new Robot(factory, jgraphPahtFinder, new CircularShape(5, 5, 2), 
+                                       new Battery(10), "Robot 1");
+        robot1.addTargetComponent(machine1);
+        robot1.addTargetComponent(machine2);
+        robot1.addTargetComponent(new Conveyor(factory, conveyorShape, "Conveyor 1"));
+        robot1.addTargetComponent(chargingStation);
 
-		final FactoryPathFinder customPathFinder = new CustomDijkstraFactoryPathFinder(factory, 5);
-		final Robot robot2 = new Robot(factory, customPathFinder, new CircularShape(45, 5, 2), new Battery(10), "Robot 2");
-		//robot2.addTargetComponent(chargingStation);
-		robot2.addTargetComponent(machine1);
-		robot2.addTargetComponent(machine2);
-		//robot2.addTargetComponent(new Conveyor(factory, conveyorShape, "Conveyor 1"));
-		
-		SwingUtilities.invokeLater(new Runnable() {
-			  
-			@Override
-	        public void run() {
-				final FileCanvasChooser canvasChooser = new FileCanvasChooser("factory", "Puck Factory");
-				final Component factoryViewer = new CanvasViewer(new SimulatorController(factory, new FactoryPersistenceManager(canvasChooser)));
-				canvasChooser.setViewer(factoryViewer);
-				//new CanvasViewer(factory);
-			}
-		});
-	}
+        final FactoryPathFinder customPathFinder = new CustomDijkstraFactoryPathFinder(factory, 5);
+        final Robot robot2 = new Robot(factory, customPathFinder, new CircularShape(45, 5, 2), new Battery(10), "Robot 2");
+        robot2.addTargetComponent(chargingStation);
+        robot2.addTargetComponent(machine1);
+        robot2.addTargetComponent(machine2);
+        robot2.addTargetComponent(new Conveyor(factory, conveyorShape, "Conveyor 1"));
+        
+        /* Invoke library for canvas */
+        SwingUtilities.invokeLater(new Runnable() {
+              
+            @Override
+            public void run() {
+                final FileCanvasChooser canvasChooser = new FileCanvasChooser("factory", "Puck Factory");
+
+                /* Add persitence manager here */
+                final Component factoryViewer         = new CanvasViewer(new SimulatorController(factory, new FactoryPersistenceManager(canvasChooser)));
+                canvasChooser.setViewer(factoryViewer);
+                //new CanvasViewer(factory);
+            }
+        });
+    }
 }
