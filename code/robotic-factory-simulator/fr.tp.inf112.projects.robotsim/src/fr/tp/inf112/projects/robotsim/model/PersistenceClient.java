284d3fea3a6dd1505949be27e57fa013f54d8362
marcutudor79 <marcutudor5@protonmail.com>
2025-11-02 16:59:19 +0100

    10-add-springboot-to-app: Decouple PersistanceManger and PersistanceClient
    - PersistanceManager should call the PersistanceClient in order
    to retrieve Canvases from the PersistanceServer
    - In this way, a PersistanceClient can be used in the springboot
    app as an interface between the Springboot and the PersistanceServer
    
    Signed-off-by: marcutudor79 <marcutudor5@protonmail.com>

---
+++ code/robotic-factory-simulator/fr.tp.inf112.projects.robotsim/src/fr/tp/inf112/projects/robotsim/model/PersistenceClient.java
@@ -0,0 +1,128 @@
+package fr.tp.inf112.projects.robotsim.model;
+
+/* Java related packages */
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.logging.Logger;
+
+/* Robotsim related packages */
+import fr.tp.inf112.projects.canvas.model.Canvas;
+
+public class PersistenceClient {
+
+    private int           port          = -1;
+    private InetAddress   netAddr       = null;
+    private SocketAddress socketAddr    = null;
+
+    Logger LOGGER = Logger.getLogger(PersistenceManager.class.getName());
+
+    /* Constructor for the PersistenceClient
+     *
+     *  @param netAddr The network address of the server
+     *  @param port    The port number of the server
+     */
+    public PersistenceClient(String netAddr, int port) {
+
+        // setup the port number
+        if (port < 0 || port > 65535) {
+            throw new IllegalArgumentException("Port number must be between 0 and 65535.");
+        }
+        this.port = port;
+
+        // try to get the InetAddress from the provided string
+        try {
+            this.netAddr = InetAddress.getByName(netAddr);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // setup the socket address
+        this.socketAddr = new InetSocketAddress(netAddr, port);
+    }
+
+    /*  Close an open socket
+     *
+     *  @param socket The socket to close
+     */
+    private void closeSocket(Socket socket)
+    {
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /* Retrieve a canvas from the server by id
+     *
+     * @param canvasId The id of the canvas to retrieve
+     * @return The canvas object retrieved from the server
+     */
+    public Canvas retrieveCanvas(final String canvasId)
+    throws IOException {
+        OutputStream outStream    = null;
+        Socket       serverSocket = new Socket();
+        Canvas       canvas       = null;
+
+        // Should be working since the socket is connected in the constructor
+        LOGGER.info("Connecting to server " + this.netAddr.toString() + " on port " + this.port + "...");
+        serverSocket.connect(this.socketAddr, 1000);
+        outStream = serverSocket.getOutputStream();
+        LOGGER.info("Connected to server.");
+
+        // send the canvasId to the server
+        LOGGER.info("Requesting canvas with id " + canvasId + "...");
+        final ObjectOutputStream objOutStream = new ObjectOutputStream(outStream);
+        objOutStream.writeObject(canvasId);
+        objOutStream.flush();
+
+        // connect to the input stream of the socket
+        ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(serverSocket.getInputStream()));
+
+        // try to read the object
+        try {
+            canvas = (Canvas) ois.readObject();
+        }
+        catch (ClassNotFoundException e) {
+            closeSocket(serverSocket);
+            throw new RuntimeException(e);
+        }
+        LOGGER.info("Canvas with id " + canvasId + " received.");
+
+        closeSocket(serverSocket);
+        return canvas;
+    }
+
+    /* Save a canvas to the server
+     *
+     * @param canvasModel The canvas model to save
+     * @return true if the canvas was saved successfully, false otherwise
+     */
+    public boolean saveCanvas(Canvas canvasModel) {
+        try (Socket serverSocket = new Socket()) {
+            LOGGER.info("Connecting to server " + this.netAddr.toString() + " on port " + this.port + "...");
+            serverSocket.connect(this.socketAddr, 1000);
+            LOGGER.info("Connected to server.");
+
+            try (OutputStream outStream = serverSocket.getOutputStream();
+                ObjectOutputStream objOutputStream = new ObjectOutputStream(outStream)) {
+
+                LOGGER.info("Sending canvas with id " + canvasModel.getId() + " to server...");
+                objOutputStream.writeObject(canvasModel);
+                objOutputStream.flush();
+                LOGGER.info("Canvas with id " + canvasModel.getId() + " sent to server.");
+                return true;
+            }
+        } catch (IOException e) {
+            LOGGER.severe("Failed to save canvas " + canvasModel.getId() + " to server");
+            return false;
+        }
+    }
+}
--- code/robotic-factory-simulator/fr.tp.inf112.projects.robotsim/src/fr/tp/inf112/projects/robotsim/model/PersistenceManager.java
+++ code/robotic-factory-simulator/fr.tp.inf112.projects.robotsim/src/fr/tp/inf112/projects/robotsim/model/PersistenceManager.java
@@ -1,110 +1,42 @@
 package fr.tp.inf112.projects.robotsim.model;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+/* Java related packages */
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
+import java.util.logging.Logger;
 
+/* Robotsim related packages */
 import fr.tp.inf112.projects.canvas.model.Canvas;
 import fr.tp.inf112.projects.canvas.model.CanvasChooser;
 import fr.tp.inf112.projects.canvas.model.impl.AbstractCanvasPersistenceManager;
-import java.net.ServerSocket;
-// added for socket connection
-import java.net.SocketAddress;
-import java.util.logging.Logger;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.Reader;
-import java.io.Writer;
-import java.net.InetSocketAddress;
-import java.net.InetAddress;
-import java.net.Socket;
-
 
 public class PersistenceManager extends AbstractCanvasPersistenceManager {
 
-    // added for the connection with the server
-    private int           port          = -1;
-    private InetAddress   netAddr       = null;
-    private SocketAddress socketAddr    = null;
+    /* Used to connect to the persistence server */
+    private PersistenceClient persistenceClient = null;
 
     Logger LOGGER = Logger.getLogger(PersistenceManager.class.getName());
 
     public PersistenceManager(final CanvasChooser canvasChooser, String netAddr, int port) {
         super(canvasChooser);
 
-        // setup the port number
-        if (port < 0 || port > 65535) {
-            throw new IllegalArgumentException("Port number must be between 0 and 65535.");
-        }
-        this.port = port;
-
-        // try to get the InetAddress from the provided string
-        try {
-            this.netAddr = InetAddress.getByName(netAddr);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // setup the socket address
-        this.socketAddr = new InetSocketAddress(netAddr, port);
-    }
-
-    /*  Function used to close an open socket
-     *
-     */
-    private void closeSocket(Socket socket)
-    {
-        try {
-            socket.close();
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
+        this.persistenceClient = new PersistenceClient(netAddr, port);
     }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public Canvas read(final String canvasId)
-    throws IOException {
-        OutputStream outStream    = null;
-        Socket       serverSocket = new Socket();
-        Canvas       canvas       = null;
+    public Canvas read(final String canvasId) {
+        Canvas canvas = null;
 
-        // Should be working since the socket is connected in the constructor
-        LOGGER.info("Connecting to server " + this.netAddr.toString() + " on port " + this.port + "...");
-        serverSocket.connect(this.socketAddr, 1000);
-        outStream = serverSocket.getOutputStream();
-        LOGGER.info("Connected to server.");
-
-        // send the canvasId to the server
-        LOGGER.info("Requesting canvas with id " + canvasId + "...");
-        final ObjectOutputStream objOutStream = new ObjectOutputStream(outStream);
-        objOutStream.writeObject(canvasId);
-        objOutStream.flush();
-
-        // connect to the input stream of the socket
-        ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(serverSocket.getInputStream()));
-
-        // try to read the object
+        /* read canvas from the server using the persistenceClient */
         try {
-            canvas = (Canvas) ois.readObject();
+            canvas = this.persistenceClient.retrieveCanvas(canvasId);
+        } catch (IOException e) {
+            LOGGER.severe("Failed to retrieve canvas " + canvasId + " from server");
         }
-        catch (ClassNotFoundException e) {
-            closeSocket(serverSocket);
-            throw new RuntimeException(e);
-        }
-        LOGGER.info("Canvas with id " + canvasId + " received.");
 
-        closeSocket(serverSocket);
         return canvas;
     }
 
@@ -112,24 +44,13 @@ public class PersistenceManager extends AbstractCanvasPersistenceManager {
      * {@inheritDoc}
      */
     @Override
-    public void persist(Canvas canvasModel)
-    throws IOException {
-        OutputStream outStream    = null;
-        Socket       serverSocket = new Socket();
-
-        // Should be working since the socket is connected in the constructor
-        LOGGER.info("Connecting to server " + this.netAddr.toString() + " on port " + this.port + "...");
-        serverSocket.connect(this.socketAddr, 1000);
-        outStream = serverSocket.getOutputStream();
-        LOGGER.info("Connected to server.");
-
-        // send the canvas to the server
-        LOGGER.info("Sending canvas with id " + canvasModel.getId() + " to server...");
-        final ObjectOutputStream objOutputStream = new ObjectOutputStream(outStream);
-        objOutputStream.writeObject(canvasModel);
-        objOutputStream.flush();
-        LOGGER.info("Canvas with id " + canvasModel.getId() + " sent to server.");
-        closeSocket(serverSocket);
+    public void persist(Canvas canvasModel) {
+        /* save the canvas to the server using persistenceClient */
+        try {
+            this.persistenceClient.saveCanvas(canvasModel);
+        } catch (Exception e) {
+            LOGGER.severe("Failed to persist canvas " + canvasModel.getId() + " to server");
+        }
     }
 
     /**
